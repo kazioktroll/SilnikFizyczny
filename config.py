d839ababcec89ec031dc672ff4520c2457c79bcb
Kazioktroll <03woj.kwi03@gmail.com>
2023-01-17 14:10:02 +0100

    Added Game() and Display() classes, program now displays a window

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 HEIGHT = 720
 WIDTH = 1080
+FPS = 60
+
+# Colors:
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+gray = (150, 150, 150)
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-import pygame
+from display import Display
+from game import Game
 from config import *
+
 
 class Main(object):
     def __init__(self):
-        self.screen = pygame.display.set_mode(WIDTH, HEIGHT)
+        # Display() is responsible for handling front-end
+        # Game() is responsible for handling back-end
+        self.display = Display()
+        self.game = Game()
+        # self.run() is the main function of the Main() class, inside happens everything
+        self.run()
+
+    def run(self):
+        while True:
+            self.game.tick()
+            self.display.draw()
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     Main()
